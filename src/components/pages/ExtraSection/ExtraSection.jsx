5ae2370291e209364a1490523ca2e2ecde23ca4c
Ram-Roy15 <ramroycombd.com>
2024-06-09 17:21:24 +0600

    corrosoul card added

--- src/components/pages/ExtraSection/ExtraSection.jsx
+++ src/components/pages/ExtraSection/ExtraSection.jsx
@@ -1,7 +1,24 @@
+import * as React from "react";
+import dayjs from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+
 const ExtraSection = () => {
+  const [value, setValue] = React.useState(dayjs());
   return (
-    <div>
-      <h1>One Extra section</h1>
+    <div className="w-full flex items-center justify-center">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DemoContainer components={["DateCalendar", "DateCalendar"]}>
+          <DemoItem label="Controlled calendar">
+            <DateCalendar
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+            />
+          </DemoItem>
+        </DemoContainer>
+      </LocalizationProvider>
     </div>
   );
 };
